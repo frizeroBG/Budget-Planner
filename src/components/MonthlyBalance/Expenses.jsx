95203a2e54237e0d8165b1530a7cc2809d54914e
frizero <kovni@abv.bg>
2017-12-01 23:52:32 +0200

    fixed payment date bug in Monthly Balance

--- src/components/MonthlyBalance/Expenses.jsx
+++ src/components/MonthlyBalance/Expenses.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 export default (props) => {
-  props = props.props
+  props = props.props;
+  let date = props.date + '/' + props.month + '/' + props.year;
+  console.log(date)
   return(
     <tr>
       <td>{props.name}</td>
       <td>{props.category}</td>
       <td>{props.amount}</td>
-      <td>{props.creationTime}</td>
+      <td>{date}</td>
       <td>
         <a href={"/expense/" + props.id} className="btn btn-secondary">Delete</a>
       </td>
